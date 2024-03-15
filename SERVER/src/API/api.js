3f489279f8cc4a931b89bb5540575293923f98ad
Quax2013 <robin.alex.ernst@gmail.com>
2024-03-15 08:33:43 +0100

    Added API base api calls

--- SERVER/src/API/api.js
+++ SERVER/src/API/api.js
@@ -1,9 +1,34 @@
 const http = require("http")
+const fs = require("fs/promises")
 const port = 8080
 
-const server = http.createServer(function (req, res) {
-    res.write('Hello World')
-    res.end()
+const server = http.createServer(async (req, res) => {
+
+    const route = req.url.replace(/^\/|\/$/g, '')
+
+    switch (route) {
+
+        case '':
+            try {
+                const index = await fs.readFile("../../assets/index.html");
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.write(index);
+                res.end();
+            } catch {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end();
+            }
+            
+            break;
+
+        case 'coffee':
+            res.statusCode = 418;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end();
+            break;
+    }
 })
 
 server.listen(port, function (error) {
